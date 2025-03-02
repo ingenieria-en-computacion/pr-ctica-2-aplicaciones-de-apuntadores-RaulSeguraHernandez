c245320c4a73da8cf8b9126cc89efac07c0efe84
RaulSeguraHernandez <raul3ro.segura@gmail.com>
2025-03-01 21:22:34 -0600

    Create e1.c

---
+++ e1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main () {
+  char *ap, c, d, e;
+  c = 'x';
+  d = 'z';
+  e = '\t';
+  
+  //Escrib el código para imprimir las direcciones de a, b y c
+  printf("%p\n%p\n%p\n", &c, &d, &e);
+  ap = &c;
+  // imprime el carácter de la localidad a la que apunta
+  printf("Carácter: %c\n",*ap);
+  // imprime el código ASCII de la localidad a la que apunta
+  printf("Código ASCII: %d\n",*ap);
+  // imprime la dirección de memoria de la localidad a la que apunta
+  printf("Dirección de memoria: %p\n",ap);
+
+  ap = &d;
+  // imprime el carácter de la localidad a la que apunta
+  printf("Carácter: %c\n",*ap);
+  // imprime el código ASCII de la localidad a la que apunta
+  printf("Código ASCII: %d\n",*ap);
+  // imprime la dirección de memoria de la localidad a la que apunta
+  printf("Dirección de memoria: %p\n",ap);
+
+  ap = &e;
+  // imprime el carácter de la localidad a la que apunta
+  printf("Carácter: %c\n",*ap);
+  // imprime el código ASCII de la localidad a la que apunta
+  printf("Código ASCII: %d\n",*ap);
+  // imprime la dirección de memoria de la localidad a la que apunta
+  printf("Dirección de memoria: %p\n",ap);
+
+ return 0;
+}
